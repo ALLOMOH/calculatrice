6b4de5b990c1f1a66e8156648522435a23543837
ALLOMOH <ALLOMOH@users.noreply.github.com>
2023-12-31 01:30:29 +0000

    time1

--- asset/index.js
+++ asset/index.js
@@ -2,67 +2,69 @@ const calculatorPromise = import("./calculator.js");
 
 let output = document.querySelector("#output");
 const equal = document.querySelector(".equal");
-const reset = document.querySelector('.reset');
+const reset = document.querySelector(".reset");
 
 let firstNumber = 0;
 let operation = null;
 let resetAfterOperation = false;
 
 document.querySelectorAll(".number").forEach((button) => {
-    button.addEventListener("click", (e) => {
-        let value = e.currentTarget.textContent;
-        if (resetAfterOperation) {
-            output.value = value;
-            resetAfterOperation = false;
-        } else {
-            output.value += value;
-        }
-    });
+  button.addEventListener("click", (e) => {
+    let value = e.currentTarget.textContent;
+    if (resetAfterOperation) {
+      output.value = value;
+      resetAfterOperation = false;
+    } else {
+      output.value += value;
+    }
+  });
 });
 
 document.querySelectorAll(".operation").forEach((button) =>
-    button.addEventListener("click", (e) => {
-        firstNumber = Number.parseFloat(output.value);
-        operation = e.currentTarget.dataset.action;
-        resetAfterOperation = true;
-    })
+  button.addEventListener("click", (e) => {
+    firstNumber = Number.parseFloat(output.value);
+    operation = e.currentTarget.dataset.action;
+    resetAfterOperation = true;
+  })
 );
 
 reset.addEventListener("click", () => {
-    output.value = null;
-})
+  output.value = null;
+});
 
 equal.addEventListener("click", () => {
-    if (!operation) {
-        return;
-    }
-    resetAfterOperation = true;
+  if (!operation) {
+    return;
+  }
+  resetAfterOperation = true;
 
-    let secondNumber = Number.parseFloat(output.value);
+  let secondNumber = Number.parseFloat(output.value);
 
-    calculatorPromise.then(calculator => {
-        let result;
-        switch (operation) {
-            case "sum":
-                result = calculator.sum(firstNumber, secondNumber);
-                break;
-            case "subtract":
-                result = calculator.subtract(firstNumber, secondNumber);
-                break;
-            case "multiply":
-                result = calculator.multiply(firstNumber, secondNumber);
-                break;
-            case "divide":
-                result = calculator.divide(firstNumber, secondNumber);
-                break;
-            default:
-                //reset operation
-                operation = null;
-                break;
-        }
-        output.value = result;
-        operation = null;
-    }).catch(error => {
-        console.error('Error importing calculator module:', error);;
+  calculatorPromise
+    .then((calculator) => {
+      let result;
+      switch (operation) {
+        case "sum":
+          result = calculator.sum(firstNumber, secondNumber);
+          break;
+        case "subtract":
+          result = calculator.subtract(firstNumber, secondNumber);
+          break;
+        case "multiply":
+          result = calculator.multiply(firstNumber, secondNumber);
+          break;
+        case "divide":
+          result = calculator.divide(firstNumber, secondNumber);
+          break;
+        default:
+          //reset operation
+          operation = null;
+          break;
+      }
+      output.value = result;
+      operation = null;
+    })
+    .catch((error) => {
+      console.error("Error importing calculator module:", error);
     });
 });
